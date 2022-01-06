648c67e299661d1474012528c4481cfa0342a835
Felix-Blueinno <felix@blueinnotechnology.com>
2022-01-06 12:03:59 +0800

    Prepared routes navigation

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,19 @@
 
 import 'package:flutter/material.dart';
 
+import 'screens/barcode_page.dart';
+import 'screens/face_detection_page.dart';
+import 'screens/labeller_page.dart';
+import 'screens/text_reconizer_page.dart';
+
 main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
+      routes: {
+        "face_detection": (context) => const FaceDetectionPage(),
+        "barcode": (context) => const BarcodePage(),
+        "labeller": (context) => const LabellerPage(),
+        "text_recognizer": (context) => const TextRecognizerPage(),
+      },
       home: HomePage(),
     ));
 
